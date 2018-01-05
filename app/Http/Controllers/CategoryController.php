44dd3070f78986c43e9903169a8b9ce50925645d
Ravinder Bhatia <ravinder@ilscorp.com>
2018-01-05 11:00:17 -0800

    categoy contoller with auth and minor changes to layouts/app view

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+
+  public function __construct()
+  {
+    $this->middleware('auth');
+  }
+
   public function all()
   {
     return view('category/showAll');
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,9 @@
             <a class="navbar-brand" href="{{ url('/') }}">
                 {{ config('app.name', 'Laravel') }}
             </a>
+            <ul class="navbar-nav">
+                <li class="nav-item"><a href="" class="nav-link">Categories</a></li> 
+            </ul>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
