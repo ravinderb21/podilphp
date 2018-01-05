2db446651fb702f04bd8bc570b351cc9f7b9b250
Ravinder Bhatia <ravinderb221@gmail.com>
2018-01-04 21:23:50 -0800

    adding categories controller and views

---
+++ resources/views/category/showAll.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-md-8 offset-md-2">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                   All Photo Categories
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
