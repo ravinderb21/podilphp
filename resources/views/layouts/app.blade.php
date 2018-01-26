bf15e9bdb25a2ce228509d175ef0a0d8ea7f01f5
Ravinder Bhatia <ravinder@ilscorp.com>
2018-01-26 12:21:39 -0800

    Updating packages and modifying welcome page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Podil</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -18,10 +18,10 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
             <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
+                Podil
             </a>
             <ul class="navbar-nav">
-                <li class="nav-item"><a href="" class="nav-link">Categories</a></li> 
+                <li class="nav-item"><a href="/categories" class="nav-link">Categories</a></li> 
             </ul>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
